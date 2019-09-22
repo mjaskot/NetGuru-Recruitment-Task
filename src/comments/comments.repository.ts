b74b3bf1cfc525ac81a2071e50c048f8868b7798
mjaskot <jaskotmarcin1@gmail.com>
2019-09-22 17:22:42 +0200

    Quickfixes and small refactor.

--- src/comments/comments.repository.ts
+++ src/comments/comments.repository.ts
@@ -8,7 +8,6 @@ export const createComment = async (params: IComment): Promise<IComment> => {
 
 export const retrieveComment = async (commentId: string): Promise<IComment> => {
   const comment = await CommentModel.findById(commentId).exec();
-  console.log(comment);
   if (!comment) {
     throw new StatusError(404, "Comment not found");
   }
@@ -20,32 +19,53 @@ export const updateComment = async (
   commentId: string,
   message: string
 ): Promise<IComment> => {
-  const toDo = await CommentModel.findOneAndUpdate(
+  const comment = await CommentModel.findOneAndUpdate(
     {
       _id: commentId
     },
     { $set: { message } }
   );
 
-  if (!toDo) {
+  if (!comment) {
     throw new StatusError(404, "Comment not found!");
   }
 
-  return toDo;
+  return comment;
 };
 
 export const deleteComment = async (commentId: string): Promise<IComment> => {
   const comment = await CommentModel.findOneAndDelete({ _id: commentId });
 
   if (!comment) {
-    throw new StatusError(404, "ToDo not found!");
+    throw new StatusError(404, "Comment not found!");
   }
 
   return comment;
 };
 
-export const getAllComments = async (movieId?: string): Promise<IComment[]> => {
-  return movieId
-    ? CommentModel.find({ movieId }).exec()
-    : CommentModel.find({}).exec();
+export const getAllComments = async (
+  movieId?: string,
+  userId?: string
+): Promise<IComment[]> => {
+  if (movieId && userId) {
+    return CommentModel.find({ movieId, userId })
+      .lean()
+      .exec();
+  }
+
+  if (movieId && !userId) {
+    return CommentModel.find({ movieId })
+      .lean()
+      .exec();
+  }
+
+  if (!movieId && userId) {
+    return CommentModel.find({ userId })
+      .lean()
+      .exec();
+  }
+
+  return CommentModel.find({})
+    .lean()
+    .exec();
 };
